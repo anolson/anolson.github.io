e4d263b4e4528a96df6d5319ae1b0e95da150382
Andrew Olson <aolson@geezeo.com>
2013-02-21 19:23:23 -0500

    Removing puts.

--- _plugins/sorted_for.rb
+++ _plugins/sorted_for.rb
@@ -1,12 +1,10 @@
 module Jekyll
   class SortedForTag < Liquid::For
     def render(context)
-      STDOUT.puts "--> #{@collection_name}"
       sorted_collection = context[@collection_name].dup
       sorted_collection.sort_by! { |i| i.to_liquid[@attributes['sort_by']] }
 
       sorted_collection_name = "#{@collection_name}_sorted".gsub('.', '_')
-      puts "#{sorted_collection_name}"
       context[sorted_collection_name] = sorted_collection
       @collection_name = sorted_collection_name
 
